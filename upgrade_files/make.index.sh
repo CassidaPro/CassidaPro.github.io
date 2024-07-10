0e6f0a524240cfb26b71debe2f0298b2f092e1ad
bob frazier <bobf@mrp3.com>
2024-07-10 06:01:41 -0700

    modified upgrade files and index script

--- upgrade_files/make.index.sh
+++ upgrade_files/make.index.sh
@@ -2,13 +2,28 @@
 
 rm index.txt
 
-for xx in *.txz ; do 
+# ile name format
+#  Cassida_skyy_Update.platform.revision.txz
+#    or
+#  Cassida_skyy_Update.platform.revision.mod.txz
+#
+# where
+#  platform is tinkerboard or RPi
+#  revision is tyipcally YYYYMMDD with possible suffix
+#  mod is optional Customer Mod value
+
+for xx in *.txz ; do
   yy=`echo $xx | sed 's/\./ /g'`
   pp=`echo $yy | awk '{ print $2; }'`
   vv=`echo $yy | awk '{ print $3; }'`
+  zz=`echo $yy | awk '{ print $4; }'`
 
-  # for now ONLY customer mod 0
-  echo $pp $vv 0 $xx "https://CassidaPro.github.io/upgrade_files/"$xx >>index.txt
+  if test "$zz" = "txz" ; then
+    # customer mod 0
+    echo $pp $vv 0 $xx "https://CassidaPro.github.io/upgrade_files/"$xx >>index.txt
+  else
+    echo $pp $vv $zz $xx "https://CassidaPro.github.io/upgrade_files/"$xx >>index.txt
+  fi
 
 done
 
